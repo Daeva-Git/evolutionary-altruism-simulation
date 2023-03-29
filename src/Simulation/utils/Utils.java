fc5fc2f509a59a83a0b1c3464f85cfb99baa06ce
DaevaStep <davitstepanyan99@gmail.com>
2023-03-29 23:07:17 +0400

    updated utils

---
+++ src/Simulation/utils/Utils.java
@@ -0,0 +1,45 @@
+package Simulation.utils;
+
+import java.util.Random;
+
+public class Utils {
+    private static long seed;
+    public static final Random random = new Random() {
+        @Override
+        public synchronized void setSeed(long seed) {
+            super.setSeed(seed);
+            Utils.seed = seed;
+        }
+    };
+    public static long getSeed() {
+        return Utils.seed;
+    }
+
+    public static int getRandomNumber(int min, int max) {
+        return random.nextInt(max - min) + min;
+    }
+
+    public static int getRandomNumberInclusive(int min, int max) {
+        return getRandomNumber(min, max + 1);
+    }
+
+    public static boolean eventOccurred (float chance) {
+        final float random = Utils.random.nextFloat();
+        return random <= chance;
+    }
+
+    // testing utils
+    public static void test (String taskName, int iterations, Runnable task) {
+        final long start = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            task.run();
+        }
+        System.out.println("elapsed nanoseconds: " + taskName + " " + (System.nanoTime() - start));
+    }
+
+    public static long getProcessDuration (Runnable process) {
+        final long start = System.nanoTime();
+        process.run();
+        return System.nanoTime() - start;
+    }
+}
