420efc29bef681814f303259a74880c057e98738
DaevaStep <davitstepanyan99@gmail.com>
2023-03-29 23:12:57 +0400

    random seed

--- src/Simulation/Main.java
+++ src/Simulation/Main.java
@@ -1,8 +1,11 @@
 package Simulation;
 
+import Simulation.utils.Utils;
+
 public class Main {
     public static void main(String[] args) {
         Utils.random.setSeed(1000);
+        Utils.random.setSeed(Utils.random.nextInt());
 
         final Simulation simulation = new Simulation();
         simulation.setData(20, 20, 100);
